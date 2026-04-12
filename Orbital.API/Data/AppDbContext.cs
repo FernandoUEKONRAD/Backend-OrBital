5bd41fe451d3d2354fc9279d89b9989f2ece02fd
JavierAlmonacid-10 <javialmonacidtorres@gmail.com>
2026-04-12 10:52:19 -0500

    Se configuran archivos de conexion a la base de datos y el modelo usuario

--- Orbital.API/Data/AppDbContext.cs
+++ Orbital.API/Data/AppDbContext.cs
@@ -5,12 +5,12 @@ namespace Orbital.API.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) 
-            : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
         public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<Planeta> Planetas { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Jerarquia> Jerarquias { get; set; }
     }
 }
--- Orbital.API/Models/Usuario.cs
+++ Orbital.API/Models/Usuario.cs
@@ -2,8 +2,26 @@ namespace Orbital.API.Models
 {
     public class Usuario
     {
-        public int Id { get; set; }
-        public required string Username { get; set; }
-        public required string Password { get; set; }
+        public int Id_Usuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Contrasena_Hash { get; set; }
+
+        public int Id_Rol { get; set; }
+
+        public int Id_Jerarquia { get; set; }
+
+        public bool Activo { get; set; }
+
+        public DateTime Fecha_Registro { get; set; }
+
+        public DateTime? Ultimo_Acceso { get; set; }
+
+        public Rol Rol { get; set; }
+
+        public Jerarquia Jerarquia { get; set; }
     }
 }
