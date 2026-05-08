47c07279e93973a79fae7c552f9d3815bfd4c586
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-05-08 12:25:32 -0500

    feat(login): add secure access by jwt token (only bypass on register url)

---
+++ Orbital.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Orbital.API.DTOs;
+using Orbital.API.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Orbital.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _service;
+
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UsuarioLoginDto dto)
+        {
+            var result = await _service.Login(dto);
+
+            if (result == null)
+                return Unauthorized(result);
+
+            return Ok(result);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UsuarioCreateDto dto)
+        {
+            var usuario = await _service.Register(dto);
+            return Ok(usuario);
+        }
+    }
+}
