71e8652201e5fd78567908aaddc8bbcde6c40ded
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-05-10 12:39:27 -0500

    fix(auth): implement some generics status code message for actions (errors, acertions)

--- Orbital.API/Controllers/AuthController.cs
+++ Orbital.API/Controllers/AuthController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Orbital.API.Controllers
 {
-    [Authorize]
     [ApiController]
     [Route("api/auth")]
     public class AuthController : ControllerBase
@@ -19,22 +18,43 @@ namespace Orbital.API.Controllers
 
         [AllowAnonymous]
         [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Login(UsuarioLoginDto dto)
         {
             var result = await _service.Login(dto);
 
             if (result == null)
-                return Unauthorized(result);
+                return StatusCode(400, new
+                {
+                    message = "credenciales incorrectas, intenta de nuevo"
+                });
 
-            return Ok(result);
+            return StatusCode(200, result);
         }
 
         [AllowAnonymous]
         [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Register(UsuarioCreateDto dto)
         {
-            var usuario = await _service.Register(dto);
-            return Ok(usuario);
+            try
+            {
+                var usuario = await _service.Register(dto);
+                return CreatedAtAction(nameof(Register), new { id = usuario.Id_Usuario }, usuario);
+            } catch (InvalidOperationException ex) {
+                return BadRequest(new{
+                    message = ex.Message
+                });
+            } catch (Exception)
+            {
+                return StatusCode(500, new
+                {
+                    message = "Ocurrió un error al registrar el usuario. Intenta de nuevo."
+                });
+            }
         }
     }
 }
