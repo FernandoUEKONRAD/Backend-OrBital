7f0a21eba7184d50273c951000f9942b950c94b4
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-05-10 11:57:10 -0500

    fix(jwt): replace default claim.Types.Role by our custom Id_rol policy label

--- Orbital.API/Services/AuthService.cs
+++ Orbital.API/Services/AuthService.cs
@@ -79,7 +79,7 @@ namespace Orbital.API.Services
             {
         new Claim(JwtRegisteredClaimNames.Sub, usuario.Id_Usuario.ToString()),
         new Claim(JwtRegisteredClaimNames.Email, usuario.Correo),
-        new Claim(ClaimTypes.Role, usuario.Id_Rol.ToString())
+        new Claim("Id_Rol", usuario.Id_Rol.ToString())
     };
 
             var token = new JwtSecurityToken(
