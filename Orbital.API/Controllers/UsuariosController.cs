a0bbd27972faee671cfc2b774017985809b3b095
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-05-10 19:15:12 -0500

    feat(user): include endpoint for get las 3 user registed

--- Orbital.API/Controllers/UsuariosController.cs
+++ Orbital.API/Controllers/UsuariosController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace Orbital.API.Controllers
 {
     [ApiController]
-    [Route("api/usuarios")]
+    [Route("api/[controller]")]
     public class UsuariosController : ControllerBase
     {
         private readonly IUsuarioService _service;
@@ -21,6 +21,14 @@ namespace Orbital.API.Controllers
         {
             var usuarios = await _service.GetUsuarios();
             return Ok(usuarios);
+        }
+
+        [Authorize(Policy = "EmperadorOnly")]
+        [HttpGet("/ultimos")]
+        public async Task<IActionResult> ObtenerUltimos3UsuariosPorRol()
+        {
+            var result = await _service.ObtenerUltimos3UsuariosPorRol();
+            return Ok(result);
         }
     }
 }
